8967385909305ff745aa395a7553cace84544a41
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2024-11-15 15:10:49 +0800

    使用建構子進行第三個員工的初始化

--- MyConsole/TestEmployeesMethod.cs
+++ MyConsole/TestEmployeesMethod.cs
@@ -31,14 +31,24 @@ namespace MyConsole
 			emp2.phone = "0912345678"; //設定員工電話
 			emp2.birthdate = new DateTime(2002, 1, 1); //設定員工生日
 
+			//使用建構子進行初始化 設定員工編號 姓名 地址
+			Employees emp3 = new Employees(3, "Tom", "Taipei")
+			{
+				phone = "0911222333",
+				birthdate = new DateTime(2003, 10, 5),
+				salary = 30000
+			}; //Object Initializer 物件初始化器
+
 
 			//核算薪水 呼叫物件方法
 			emp1.calSalary(22, 10000);
 			emp2.calSalary(22, 10000);
+			emp3.calSalary(22, 10000);
 
 			//輸出
 			Console.WriteLine($"編號:{emp1.id} 姓名:{emp1.name} 底薪:{emp1.salary} 實際薪資:{emp1.actSalary} 生日:{emp1.birthdate}");
 			Console.WriteLine($"編號:{emp2.id} 姓名:{emp2.name} 底薪:{emp2.salary} 實際薪資:{emp2.actSalary} 生日:{emp2.birthdate}");
+			Console.WriteLine($"編號:{emp3.id} 姓名:{emp3.name} 底薪:{emp3.salary} 實際薪資:{emp3.actSalary} 生日:{emp3.birthdate}");
 		}
 	}
 }
