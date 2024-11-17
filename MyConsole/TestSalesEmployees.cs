64428729506dbcac90131757e564a59ec0ad5254
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2024-11-17 20:32:05 +0800

    輸出

--- MyConsole/TestSalesEmployees.cs
+++ MyConsole/TestSalesEmployees.cs
@@ -28,9 +28,18 @@ namespace MyConsole
             }; //物件初始化
 
 
+            //業務實際業績
+            sales1.ActSalesAmount = 1000000;
+            //業績獎金核算
+            sales1.calCommision();
+            //核算薪資
+            emp1.calSalary(22, 10000);
+            sales1.calSalary(22, 10000); //繼承下來的方法，在子類別是否OK? (方法容易改版)
+
+
             //輸出
-            Console.WriteLine($"編號:{emp1.id} 姓名:{emp1.name} 底薪:{emp1.salary} 生日:{emp1.birthdate}");
-            Console.WriteLine($"編號:{sales1.id} 姓名:{sales1.name} 底薪:{sales1.salary} 生日:{sales1.birthdate} 業績:{sales1.SalesAmount}");
+            Console.WriteLine($"編號:{emp1.id} 姓名:{emp1.name} 底薪:{emp1.salary} 生日:{emp1.birthdate} 實際薪資:{emp1.actSalary}");
+            Console.WriteLine($"編號:{sales1.id} 姓名:{sales1.name} 底薪:{sales1.salary} 生日:{sales1.birthdate} 業績:{sales1.SalesAmount} 實際業績:{sales1.ActSalesAmount} 業績獎金:{sales1.Commission} 實際薪資:{sales1.actSalary}");
         }
     }
 }
