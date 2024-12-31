e7e32c1fcdfa9ea79cf1183f776b359b1b317702
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2024-12-31 21:34:16 +0800

    自訂建構子

--- MyConsole/Models/Engineer.cs
+++ MyConsole/Models/Engineer.cs
@@ -7,7 +7,20 @@ using System.Threading.Tasks;
 //工程師是員工
 namespace MyConsole.Models
 {
+    //繼承父類別Employees
     public class Engineer: Employees //Engineer繼承Employees
     {
+        //Data Field
+        private Int32 _techBons; //技術津貼
+        private String _tech; //技術
+
+        //自訂建構子
+        public Engineer(Int32 id, String name, String address, Int32 salary, String tech, Int32 techBons):base(id, name, address) { 
+            
+            //設定底薪
+            this.salary = salary; //父親屬性salary進行設定
+            this._tech = tech;
+            this._techBons = techBons;
+        }
     }
 }
