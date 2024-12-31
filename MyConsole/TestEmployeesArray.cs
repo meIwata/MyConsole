e5a18a40941aaa42e626a466455cab9f4a8f6c52
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-01 02:13:38 +0800

    TestEmployeesArray

---
+++ MyConsole/TestEmployeesArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//收集各類型的員工們(陣列) Employees/Engineer/Sales
+namespace MyConsole
+{
+    internal class TestEmployeesArray
+    {
+        //Entry Point
+        public void Main() { 
+        
+        }
+    }
+}
