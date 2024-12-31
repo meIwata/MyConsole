3132870665052cc8433d8d9f4a9aeb9538e3d575
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2024-12-31 21:23:21 +0800

    新增Engineer子類別

---
+++ MyConsole/Models/Engineer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//工程師是員工
+namespace MyConsole.Models
+{
+    public class Engineer: Employees //Engineer繼承Employees
+    {
+    }
+}
