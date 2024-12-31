7cb3176989e15afa823f48719048c3ffc047346d
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2024-12-31 21:53:24 +0800

    Overriding

--- MyConsole/Models/Engineer.cs
+++ MyConsole/Models/Engineer.cs
@@ -23,9 +23,16 @@ namespace MyConsole.Models
             this._techBons = techBons;
         }
 
-
         //Property設定
         public int TechBons { get => _techBons; set => _techBons = value; } //可以設定也可以取得
         public string Tech { get => _tech; set => _tech = value; }
+
+        //Overiding(四大一樣  修飾詞/回應值/方法名稱 /參數列  父親薪資核算 加上技術津貼
+        public override void calSalary(int days, int bonus)
+        {
+            base.calSalary(days, bonus);
+            //加上技術津貼
+            this._actSalary += this._techBons;
+        }
     }
 }
