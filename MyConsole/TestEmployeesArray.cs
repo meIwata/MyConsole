8dfac6001e7ab8991d4beddaf9f7a004416383d5
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-01 19:11:05 +0800

    calEmployeesSalary

--- MyConsole/TestEmployeesArray.cs
+++ MyConsole/TestEmployeesArray.cs
@@ -55,21 +55,33 @@ namespace MyConsole
                     ((Sales)emp).calCommision();
                 }
 
+                //不管是哪一種員工 通通要薪資核算 class Member (static method)
+                //calEmployeesSalary(emp, 22, 10000); //寫成這樣也可以因為都在同一個TestEmployeesArray類別裡面
+                TestEmployeesArray.calEmployeesSalary(emp, 22, 10000);
+
+
                 //輸出
                 if (emp is Sales)
                 { //如果是業務員
                     Sales s1 = (Sales)emp; //轉型成Sales
-                    Console.WriteLine($"編號:{s1.id} 姓名:{s1.name} 底薪:{s1.salary}  生日:{s1.birthdate} 實際業績:{s1.ActSalesAmount} 業績獎金:{s1.Commission}");
+                    Console.WriteLine($"編號:{s1.id} 姓名:{s1.name} 底薪:{s1.salary}  生日:{s1.birthdate} 實際業績:{s1.ActSalesAmount} 業績獎金:{s1.Commission} 實際薪水:{s1.actSalary}");
                 }
                 else if (emp is Engineer)
                 { //如果是工程師
                     Engineer s1 = (Engineer)emp; //轉型成Engineer
-                    Console.WriteLine($"編號:{s1.id} 姓名:{s1.name} 底薪:{s1.salary}  生日:{s1.birthdate} 技術工作:{s1.Tech} 技術津貼:{s1.TechBons}");
+                    Console.WriteLine($"編號:{s1.id} 姓名:{s1.name} 底薪:{s1.salary}  生日:{s1.birthdate} 技術工作:{s1.Tech} 技術津貼:{s1.TechBons} 實際薪水:{ s1.actSalary}");
                 }
                 else {
-                    Console.WriteLine($"編號:{emp.id} 姓名:{emp.name} 底薪:{emp.salary}  生日:{emp.birthdate}");
+                    Console.WriteLine($"編號:{emp.id} 姓名:{emp.name} 底薪:{emp.salary}  生日:{emp.birthdate} 實際薪水:{emp.actSalary}");
                 }
             }
+        }
+
+        //static method
+        public static void calEmployeesSalary(Employees emp, Int32 days, Int32 bons) {
+
+            //通稱是員工(執行本尊可能Sales或者Engineer)
+            emp.calSalary(days, bons); //進行VMI(Virtual Method Invocation)虛擬方法呼叫(子類別改版的功能)
         }
     }
 }
