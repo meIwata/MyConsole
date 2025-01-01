38874cfa50c25832c5ee25726c9ef0698c113a26
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-01 22:01:55 +0800

    新增Customers、CustomersDao

---
+++ MyConsole/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+////無須屬性設定邏輯 採用自動屬性(屬性寫成抽象 沒有設定Data Field) 編譯自動實作屬性與產生對應資料欄位
+namespace MyConsole.Models
+{
+    public class Customers
+    {
+        //原本應該要這樣寫
+        /*
+        private String _customerId;
+        public string CustomerId { get => _customerId; set => _customerId = value; }
+        */
+
+        //現在寫成抽象屬性 自動實作屬性（Auto-Implemented Properties）
+        public String customerId { get; set; } //顧客編號
+        public String companyName { get; set; } //公司名稱
+        public String address { get; set; } //公司地址
+        public String phone { get; set; } //公司電話
+        public String country { get; set; } //公司國家
+        //這再編譯的時候會產生資料欄
+    }
+}
---
+++ MyConsole/Models/CustomersDao.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole.Models
+{
+    public class CustomersDao : IDao<Customers, String>
+    {   
+        //Data Field
+        private IDbConnection _connection;
+        public IDbConnection Connection{
+            
+            //傳統的 get 和 set
+            /*
+            set {
+                _connection = value;
+            }
+            get { 
+                return _connection; 
+            */
+
+            //Lambda語法
+            set => _connection = value;
+            get => _connection;
+        }
+
+        public bool delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customers get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customers> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool insert(Customers entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(Customers entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
