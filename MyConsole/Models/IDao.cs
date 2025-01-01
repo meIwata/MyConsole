33d8b9fa189be8398237d4acce95dbfe1f7def33
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-01 19:42:50 +0800

    新增介面IDao

---
+++ MyConsole/Models/IDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//規範對資料CRUD的操作(Creat, Read, Update, Delete)
+namespace MyConsole.Models
+{
+    internal interface IDao
+    {
+
+    }
+}
