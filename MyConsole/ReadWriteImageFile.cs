1d41a6f3d4f8b098dd7d587c4a2ffd660460581a
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-02-01 00:05:57 +0800

    System.IO - 讀取檔案，再把檔案寫出去

--- MyConsole/ReadWriteImageFile.cs
+++ MyConsole/ReadWriteImageFile.cs
@@ -20,8 +20,10 @@ namespace MyConsole
             String path = $@"C:\Users\lulul\Downloads\{fileName}.jpg"; // \ escape character
 
             Stream fs = null; //要給預設值，因為他不一定開啟成功
+            Stream outFs = null;
             //開啟
-            try {
+            try
+            {
                 //開啟檔案Stream
                 fs = new FileStream(path, FileMode.Open, FileAccess.Read);
 
@@ -31,25 +33,46 @@ namespace MyConsole
                 //讀取檔案至緩衝區
                 fs.Read(buffer, 0, buffer.Length); //從第一個位置讀到最後一個位置。但這裡如果奘到例外不能讀取...
 
-                
+
                 Console.WriteLine($"檔案讀取成功...{buffer.Length}");
-                //寫出去...
+                //寫出去...建立FileStream方向開啟 建立/同時具有開啟  (這邊檔案也是放在跟來源一樣的地方，先建立的名稱取名XXX_copy)
+                outFs = new FileStream($@"C:\Users\lulul\Downloads\{fileName}_copy.jpg", FileMode.Create, FileAccess.Write);
 
+                //寫入檔案
+                outFs.Write(buffer, 0, buffer.Length);
 
-            } catch (FileNotFoundException ex) { //子類別先寫
+                //清除緩存區
+                outFs.Flush();
+
+                //關閉檔案也是要寫到finally去
+
+            }
+            catch (FileNotFoundException ex)
+            { //子類別先寫
                 Console.WriteLine("檔案不存在");
 
-            } catch (IOException ex) { //父類別放後面
+            }
+            catch (IOException ex)
+            { //父類別放後面
                 Console.WriteLine("檔案存取錯誤");
 
             }
-            finally { //正常或者產生例外也要到這裡
+            finally
+            { //正常或者產生例外也要到這裡
                 //關閉檔案
-                if (fs != null) {
+                if (fs != null) //不是空值進行關閉
+                {
                     Console.WriteLine("關閉檔案");
                     fs.Close();
-                } 
+                }
+
+                //關閉檔案
+                if (outFs != null) //不是空值進行關閉
+                {
+                    Console.WriteLine("關閉輸出檔案");
+                    outFs.Close();
+                }
             }
-        }
+        }   
     }
 }
