fd7a6e4ccb820c40754a6389c48e4b5ac75985a1
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-31 19:57:13 +0800

    自訂Method具有例外管理

--- MyConsole/Models/Math.cs
+++ MyConsole/Models/Math.cs
@@ -11,10 +11,22 @@ namespace MyConsole.Models
         //除法運算 無須個別物件操作(參數進來 處理之後 回應值)
         //satatic 關鍵字 無須個別資訊處理 static 直接抽出來 進行執行
         //class Member(Non Instance Method)
-        public static Int32 Division(Int32 v, Int32 b) { 
-            //進行除法運算
-            Int32 result = v / b;
-            return result;
+        //設定第三個參數 將回應值改換 By Reference(傳址) 參數
+        public static Boolean Division(Int32 v, Int32 b, out Int32 result) {
+
+            Boolean resultFlag = false;
+            //進行例外管理(分母是0)
+            try {
+                //進行除法運算
+                result = v / b; //這裡也可能產生例外 除數為0
+                resultFlag = true; //運算成功就是true
+
+            }
+            catch (DivideByZeroException ex) {
+                //Logging...
+                result = -1;
+            }
+            return resultFlag;
         }
     }
 }
--- MyConsole/TestMathDiv.cs
+++ MyConsole/TestMathDiv.cs
@@ -21,18 +21,25 @@ namespace MyConsole
             if (Int32.TryParse(value, out v)) { //TryParse是布林值，如果value轉的過去就傳v
                 Int32 b;
                 //進行分母值轉換
-                if (Int32.TryParse(baseValue, out b)) { //如果baseValue轉的過去就傳b
-                    //進行運算
+                if (Int32.TryParse(baseValue, out b)) { //TryParse是布林值，如果baseValue轉的過去就傳b。 自訂Method具有例外管理 且進行by Reference 處理值參照
+                    //進行除法運算
                     //Math.Division(v, b); //用的是.Net的類別
-                    Int32 result = MyConsole.Models.Math.Division(v, b); //使用自己命名空間的類別
-                    
+                    Int32 result;
+                    if (MyConsole.Models.Math.Division(v, b, out result)) //自訂Method具有例外管理 且進行by Reference 處理值參照
+                    { //使用自己命名空間的類別
+                        //如果是true，輸出處理
+                        Console.WriteLine($"除法運算 {v}/{b} = {result}");
+                    }
+                    else {
+                        Console.WriteLine("分母不可為0");
+                    }
                     //輸出
-                    Console.WriteLine("答案: " + result);
+                    //Console.WriteLine("答案: " + result);
                 } else {
-                    Console.WriteLine("分母轉換失敗");
+                    Console.WriteLine("分母輸入錯誤");
                 }
             } else {
-                Console.WriteLine("分子轉換失敗");
+                Console.WriteLine("分子輸入錯誤");
             }
         }
     }
