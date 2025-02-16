c68091d3603028cc0dbce7d18ab68f67b7fb7629
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-02-16 23:43:12 +0800

    TestDirectory.cs

--- MyConsole/TestDirectory.cs
+++ MyConsole/TestDirectory.cs
@@ -19,8 +19,27 @@ namespace MyConsole
             if (Directory.Exists(fileName)) { 
                 // 存在
                 Console.WriteLine("目錄存在");
-                //找到目錄後，要開啟個體物件對應DirectoryInfo
 
+                //找到目錄後，要開啟個體物件對應DirectoryInfo
+                //建構一個DirectoryInfo物件
+                DirectoryInfo dir = new DirectoryInfo(fileName);
+                
+                //取出該目錄屬性(Attribute)
+                FileAttributes atts = dir.Attributes; //取回列舉(目錄類型)
+                Console.WriteLine("目錄屬性: " + atts);
+
+                //可以繼續查詢，查詢建立時間
+                DateTime createDate = dir.CreationTime;
+                Console.WriteLine("建立時間: " + createDate);
+
+                //取出目錄下所有檔案
+                FileInfo[] files = dir.GetFiles();
+                //走訪每一個檔案(FileInfo)物件
+                foreach (FileInfo file in files) {
+                    Console.WriteLine("檔案名稱:" + file.Name);
+                    Console.WriteLine("檔案大小:" + file.Length);
+                    Console.WriteLine("檔案建立時間:" + file.CreationTime);
+                }
             }
             else {
                 // 不存在
