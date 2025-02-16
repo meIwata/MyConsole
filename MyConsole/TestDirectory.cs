a6a61efaadc647480cdf195af0cf5847c0546848
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-02-16 22:49:28 +0800

    TestDirectory.cs

---
+++ MyConsole/TestDirectory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//操作Directory類別共用功能  DirectoryInfo 對應物件 操作相對目錄
+namespace MyConsole
+{
+    internal class TestDirectory
+    {
+        //Entry point
+        public static void Main() { 
+        
+        }
+    }
+}
